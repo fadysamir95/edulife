9b64b5d380622328a6fb97bc6d1f60e4b6105783
TaherAyman1 <66644798+TaherAyman1@users.noreply.github.com>
2024-12-02 20:42:11 +0200

    cleaning code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
-import { DiplomasModule } from './modules/diplomas/diplomas.module';
-import { MastersModule } from './modules/masters/masters.module';
-import { TotModule } from './modules/tot/tot.module';
-import { SharedModule } from './modules/shared/shared.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
 import { ErrorInterceptor } from './shared/services/error.interceptor';
-import { AuthModule } from './modules/auth/auth.module';
-import { ArticlesModule } from './modules/articles/articles.module';
-import { AboutModule } from './modules/about/about.module';
 
 @NgModule({
   declarations: [AppComponent],
