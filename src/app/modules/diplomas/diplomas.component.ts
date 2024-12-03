f7c5c3ce169c2827fb9a913955523134bd87e67c
Fady Samir <37264344+fadysamir95@users.noreply.github.com>
2024-12-03 15:41:37 +0200

    Updates

--- src/app/modules/diplomas/diplomas.component.ts
+++ src/app/modules/diplomas/diplomas.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-diplomas',
-  templateUrl: './diplomas.component.html'
+  templateUrl: './diplomas.component.html',
+  styleUrl: './diplomas.component.scss'
 })
 export class DiplomasComponent {
   parentMessage: string = "مسارات تعليمية";
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    margin: 20,
+    navText: ['<', '>'],
+    rtl: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 6
+      }
+    },
+    nav: false
+  }
 }
