6f73a5bf0cd1114ef5b03408499d5fa7ef21401e
Fady Samir <37264344+fadysamir95@users.noreply.github.com>
2024-12-05 19:34:08 +0200

    Bug Fixing

--- src/app/modules/diplomas/diplomas.component.ts
+++ src/app/modules/diplomas/diplomas.component.ts
@@ -36,7 +36,7 @@ export class DiplomasComponent implements OnInit {
       0: { items: 1 },
       400: { items: 2 },
       740: { items: 3 },
-      940: { items: 4 }
+      940: { items: 6 }
     },
     nav: true
   };
